28ef3d69755e2dd831cb3ee80aa24bda46980eaf
Zainab Abdussalam-Osimade <74374137+zao12345@users.noreply.github.com>
2024-04-14 21:53:16 -0400

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
- 
-@app.route('/')
-def hello_cloud():
-    return 'Hello from Osimade ECS Container'
- 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
